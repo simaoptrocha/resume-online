f68d20c2f8cbdd4f442469120545b58ccc7d62e0
simaoptrocha <31962981+simaoptrocha@users.noreply.github.com>
2022-09-27 01:00:59 +0100

    new folder structure

--- ui.js
+++
@@ -1,8 +0,0 @@
-function showTime(e) {
-    e.setAttribute("done", "true");
-    e.querySelector(".timeline-point").style.color = "#2e6061"
-    e.querySelector(".date").style.opacity = "100%";
-    e.querySelector("p").style.opacity = "100%";
-    e.querySelector("p").style.transform = "translateY(0px)";
-  }
-  
